141dbbf23a92f02329f71c17ae445fe766ba91a7
EduardoCavValenca <eduardo.cvalenca@gmail.com>
2023-04-06 18:34:13 -0300

    Comparing to Fake Check

--- visualization_files/results_graphs.py
+++ visualization_files/results_graphs.py
@@ -75,6 +75,30 @@ def graph_compare_datasets_accuracy(df_plot: pd.DataFrame, title: str = None):
     ax.set_title(title)
     return ax
 
+
+def graph_compare_face_check_accuracy(df_plot: pd.DataFrame, title: str = None):
+    df_plot = df_plot.reset_index()
+    df_plot = df_plot.melt(id_vars=["algorithm"], var_name="metric", value_name="accuracys")
+
+    sns.set_theme(style="whitegrid", palette="Set2", color_codes=True)
+
+    # Initialize the matplotlib figure
+    f, ax = plt.subplots(figsize=(4, 4))
+
+    # Plot the total crashes
+    sns.barplot(x="algorithm", y="accuracys", hue="metric", data=df_plot, ax=ax)
+
+    for container in ax.containers:
+        ax.bar_label(container, label_type='edge', fmt='%1.2f', color='black', size=8)
+
+    # Add a legend and informative axis label
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+            fancybox=True, shadow=True, ncol=3)
+
+    ax.set_ylim(0, 1)
+    ax.set_title(title)
+    return ax
+
 def colors_from_values(values, palette_name):
     # normalize the values to range [0, 1]
     normalized = (values - min(values)) / (max(values) - min(values))
